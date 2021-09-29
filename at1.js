5a639dac1052e75d11cc9e8f3f1a1fb120bc0fee
Luan Felipe <luan0412@outlook.com>
2021-09-29 20:40:07 -0300

    Atividades de String 2

---
+++ at1.js
@@ -0,0 +1,11 @@
+function tabuadaCinco(){
+    let tabuada = 5
+    let tabuadaArray = []
+    
+    for(var count=1; count<=10 ; count++){
+        tabuadaArray.push(tabuada * count)
+} 
+console.log(tabuadaArray)  
+}
+tabuadaCinco()
+
---
+++ at2.js
@@ -0,0 +1,7 @@
+
+let n1 = [54,78,56,98,0,12,11,37]
+let n1Resultado = 0
+for (let i = 0 ; i<n1.length; i++){
+    n1Resultado = (n1[i]-32)/1.8
+    console.log(n1Resultado)
+}
